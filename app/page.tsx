77cea2c1f2f9bd495648ac3ba90650cdf7afd3fa
Norah-Nicole <nicolenorahouma@gmail.com>
2026-07-25 13:06:18 +0300

    fix hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main>
+      <Hero />
       <h1>Welcome to Next.js!</h1>
     </main>
   );
